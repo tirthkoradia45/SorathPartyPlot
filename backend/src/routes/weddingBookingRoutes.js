5d1eef29e9b2b07fb5c40df5d8eae873755192b6
TIRTH KORADIYA <tirthkoradia7@gmail.com>
2026-07-09 19:11:05 +0530

    Change the MongoDb Atlas Password

--- backend/src/routes/weddingBookingRoutes.js
+++ backend/src/routes/weddingBookingRoutes.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
+
 const { verifyAdmin } = require("../middleware/authMiddle");
+
 const {
-    getAllWeddingBookings,
-    createWeddingBooking,
-    updateWeddingStatus,
-    deleteWeddingBooking
+  getAllWeddingBookings,
+  createWeddingBooking,
+  updateWeddingStatus,
+  deleteWeddingBooking,
 } = require("../controllers/weddingBookingController");
 
 router.get("/", verifyAdmin, getAllWeddingBookings);
 
-router.post("/",createWeddingBooking);
+router.post("/", createWeddingBooking);
 
-router.patch("/:id", verifyAdmin, updateWeddingBookingStatus);
+router.patch("/:id", verifyAdmin, updateWeddingStatus);
 
 router.delete("/:id", verifyAdmin, deleteWeddingBooking);
 
